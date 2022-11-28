d979317cec71af883b7342ef9a1fbe50fdf50799
nishimura55 <naoki.hnmr55@gmail.com>
2022-11-28 21:46:25 +0900

    feat: Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/pages/**/*.{js,ts,tsx}', './src/components/**/*.{js,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}

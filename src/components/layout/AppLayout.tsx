6ed33cc8f45c6e93d3370f626b5402380b019bee
nishimura55 <naoki.hnmr55@gmail.com>
2022-12-02 08:21:40 +0900

    refactor: Change props type name

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -2,11 +2,11 @@ import { FC, ReactNode } from 'react'
 import { Footer } from '../Footer'
 import { Header } from '../Header'
 
-type Props = {
+type AppLayoutProps = {
   children: ReactNode
 }
 
-export const AppLayout: FC<Props> = ({ children }) => {
+export const AppLayout: FC<AppLayoutProps> = ({ children }) => {
   return (
     <div className='mx-auto flex h-screen max-w-pageContent flex-col content-between bg-gray-100'>
       <Header />

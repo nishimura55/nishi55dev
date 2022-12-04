85ac5eb81ab0bfdda1d502f23e7b9468c7fa421b
nishimura55 <naoki.hnmr55@gmail.com>
2022-12-04 16:03:29 +0900

    feat: Tagname injectable

--- src/components/common/PageTitle.tsx
+++ src/components/common/PageTitle.tsx
@@ -2,8 +2,9 @@ import { FC } from 'react'
 
 type PageTitleProps = {
   title: string
+  tagName?: 'h1' | 'h2'
 }
 
-export const PageTitle: FC<PageTitleProps> = ({ title }) => {
-  return <h1 className='mb-5 text-center text-3xl'>{title}</h1>
+export const PageTitle: FC<PageTitleProps> = ({ title, tagName: HeaderTag = 'h1' }) => {
+  return <HeaderTag className='mb-5 text-center text-3xl'>{title}</HeaderTag>
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,8 @@ import { PageTitle } from '../components/common/PageTitle'
 const Home: NextPage = () => {
   return (
     <div className='h-full bg-blue-300 p-4 text-white'>
-      <PageTitle title='Blog post' />
+      <h1 className='absolute h-0 w-0 opacity-0'>nishiのブログページ</h1>
+      <PageTitle title='Blog post' tagName='h2' />
       {/* TODO: ブログ一覧を表示 */}
       <div className='mt-4'>coming soon...</div>
     </div>

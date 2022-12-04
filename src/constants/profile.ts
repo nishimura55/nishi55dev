d76c97fd1182256481510e3506a66c942ba84f63
nishimura55 <naoki.hnmr55@gmail.com>
2022-12-04 16:54:16 +0900

    feat: Update profile page

---
+++ src/constants/profile.ts
@@ -0,0 +1,6 @@
+export const skillNames = ['JavaScript', 'TypeScript', 'React', 'Next.js', 'Ruby', 'Ruby on Rails']
+
+export const links = [
+  { name: 'github', url: 'https://github.com/nishimura55' },
+  { name: 'twitter', url: 'https://twitter.com/nishi55dev' },
+]
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -2,18 +2,45 @@ import { NextPage } from 'next'
 import Image from 'next/image'
 import { PageTitle } from '~/src/components/common/PageTitle'
 import ProfileImage from '~/public/sample-image.png'
+import { SocialIcon } from 'react-social-icons'
+import { links, skillNames } from '~/src/constants/profile'
 
 const Profile: NextPage = () => {
   return (
     <div className='h-full bg-blue-300 p-4 text-white'>
       <PageTitle title='Profile' />
-      <div className='flex flex-col items-center gap-4'>
+      <div className='flex flex-col items-center gap-5 text-center'>
         <Image
           src={ProfileImage}
           alt='プロフィール画像'
           className='h-32 w-32 rounded-full sm:h-40 sm:w-40'
         />
-        <p>Software Engineer</p>
+        <div>
+          <h2 className='mb-1'>Name</h2>
+          <p className='text-sm'>nishi</p>
+        </div>
+        <div>
+          <h2 className='mb-1'>Job</h2>
+          <p className='text-sm'>Software Engineer</p>
+        </div>
+        <div>
+          <h2 className='mb-1'>Skills</h2>
+          <ul className='flex flex-wrap justify-center gap-3 text-sm'>
+            {skillNames.map((skillName, index) => (
+              <li key={index}>{skillName}</li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <h2 className='mb-1'>Links</h2>
+          <ul className='flex flex-wrap justify-center gap-3 text-sm'>
+            {links.map((link) => (
+              <li key={link.url}>
+                <SocialIcon target='_blank' url={link.url} style={{ height: 32, width: 32 }} />
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   )

242163407688baa1cb7e14dd0ec32800b1b9f7eb
nishimura55 <naoki.hnmr55@gmail.com>
2022-12-04 15:49:58 +0900

    feat: Profile page

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,11 +1,20 @@
 import { NextPage } from 'next'
+import Image from 'next/image'
 import { PageTitle } from '~/src/components/common/PageTitle'
+import ProfileImage from '~/public/sample-image.png'
 
 const Profile: NextPage = () => {
   return (
     <div className='h-full bg-blue-300 p-4 text-white'>
       <PageTitle title='Profile' />
-      <div>Software Engineer</div>
+      <div className='flex flex-col items-center gap-4'>
+        <Image
+          src={ProfileImage}
+          alt='プロフィール画像'
+          className='h-32 w-32 rounded-full sm:h-40 sm:w-40'
+        />
+        <p>Software Engineer</p>
+      </div>
     </div>
   )
 }

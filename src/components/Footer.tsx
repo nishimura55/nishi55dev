c6711fed1801ada7db836354e8555f50fe3340be
nishimura55 <naoki.hnmr55@gmail.com>
2022-12-05 20:57:01 +0900

    feat: Arrange footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export const Footer: FC = () => {
   return (
     <footer>
       <div className='flex justify-center bg-gray-300 py-4 text-sm'>
-        <div>2022</div>
+        <div className='text-gray-500'>&copy; 2022 nishi</div>
       </div>
     </footer>
   )

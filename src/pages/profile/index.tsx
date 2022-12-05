7ad9efdb3d893564f7e183b5ac60b44d1ae0f9bb
nishimura55 <naoki.hnmr55@gmail.com>
2022-12-05 20:39:23 +0900

    feat: Arrange style

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -7,24 +7,24 @@ import { links, skillNames } from '~/src/constants/profile'
 
 const Profile: NextPage = () => {
   return (
-    <div className='h-full bg-blue-300 p-4 text-white'>
+    <div className='h-full bg-blue-300 p-4 pb-8 text-white'>
       <PageTitle title='Profile' />
-      <div className='flex flex-col items-center gap-5 text-center'>
+      <div className='flex flex-col items-center gap-7 text-center'>
         <Image
           src={ProfileImage}
           alt='プロフィール画像'
           className='h-32 w-32 rounded-full sm:h-40 sm:w-40'
         />
         <div>
-          <h2 className='mb-1'>Name</h2>
+          <h2 className='mb-2'>Name</h2>
           <p className='text-sm'>nishi</p>
         </div>
         <div>
-          <h2 className='mb-1'>Job</h2>
+          <h2 className='mb-2'>Job</h2>
           <p className='text-sm'>Software Engineer</p>
         </div>
         <div>
-          <h2 className='mb-1'>Skills</h2>
+          <h2 className='mb-2'>Skills</h2>
           <ul className='flex flex-wrap justify-center gap-3 text-sm'>
             {skillNames.map((skillName, index) => (
               <li key={index}>{skillName}</li>
@@ -32,7 +32,7 @@ const Profile: NextPage = () => {
           </ul>
         </div>
         <div>
-          <h2 className='mb-1'>Links</h2>
+          <h2 className='mb-2'>Links</h2>
           <ul className='flex flex-wrap justify-center gap-3 text-sm'>
             {links.map((link) => (
               <li key={link.url}>

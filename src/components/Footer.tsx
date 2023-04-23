f6cd1fe8ee47a83f106defec806875d37aa5c780
nishimura55 <naoki.hnmr55@gmail.com>
2023-04-23 09:25:59 +0900

    update to 2023

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <footer>
       <div className='flex justify-center bg-gray-300 py-4 text-sm'>
-        <div className='font-sans text-gray-500'>&copy; 2022 nishi</div>
+        <div className='font-sans text-gray-500'>&copy; 2023 nishi</div>
       </div>
     </footer>
   )

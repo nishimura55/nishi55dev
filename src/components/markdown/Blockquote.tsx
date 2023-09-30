790af5b82f0f88f9ed9a80988cb356cfbe4ebb16
nishimura55 <naoki.hnmr55@gmail.com>
2023-09-30 17:26:20 +0900

    improve contrast

--- src/components/markdown/Blockquote.tsx
+++ src/components/markdown/Blockquote.tsx
@@ -2,7 +2,7 @@ import { Components } from 'react-markdown'
 
 export const Blockquote: Components['blockquote'] = ({ children }) => {
   return (
-    <blockquote className='my-4 whitespace-pre-wrap rounded bg-gray-50 px-4 font-sans text-gray-400'>
+    <blockquote className='my-4 whitespace-pre-wrap rounded bg-gray-50 px-4 font-sans text-gray-500'>
       {children}
     </blockquote>
   )
